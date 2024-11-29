8d69976b2d0d5df31dfb3fae2050b284328cd8bd
Alex <howdy140@gmail.com>
2024-11-29 02:34:13 -0500

    endpoint redirect test

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello, world!').end();
 });
 
+app.get('/following_verify', (req, res) => {
+    res.status(200).send('/following_verify hit successful!').end();
+});
+
+app.get('/auth_redirect', (req, res) => {
+    console.log(req);
+    res.status(200).send('Hello, world!').end();
+});
+
 // Start the server
 const PORT = parseInt(process.env.PORT) || 8080;
 app.listen(PORT, () => {
